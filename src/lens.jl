204299933b03d57e9440ab8b3c6d5eee0af36a13
Travis Deane Ashworth <travisashworth2007@gmail.com>
2017-09-07 15:16:21 -0500

    Update lens.jl

--- src/lens.jl
+++ src/lens.jl
@@ -1,4 +1,6 @@
-
+module lens
+using ImageView, Images, TestImages
+using ImageFiltering, Colors, ImageMetadata, FixedPointNumbers
 
 # include("Utility.jl")
 # include("Draw.jl")
@@ -7,10 +9,11 @@
 # a few convenience variables...
 # NOTE: be sure to change the directory if needed
 #    blueGalaxy.jpg    Galaxy.JPG    Spiral.jpg
-     buffer = load("/lens/images/Galaxy.JPG")
-     imgc = load("/lens/images/Galaxy.JPG")
-     width, height = size(imgc)
-     halfWidth, halfHeight = round(Int64, width*.5), round(Int64, height*.5)
+    currentPath = pwd()
+    buffer = load(currentPath * "/lens/images/Galaxy.JPG")
+    imgc = fill(RGB{N0f8}(0,0,0),(1575, 2800)) # rand(RGB{N0f8}, 1575, 2800)
+    width, height = size(buffer)
+    halfWidth, halfHeight = round(Int64, width*.5), round(Int64, height*.5)
 # ==============================================================================
 #                                  CURRENT METHOD:
 # ------------------------------------o--------------------p--------------------    <-- source image
@@ -21,7 +24,7 @@
 # Above you can see an example of a pixel (p) at distance (d) from origion (o)
 # in the line representing the altered image p has been displaced. How is that
 # displacement value calculated? ...logically it must be a value that is relative
-# to the distance. I am using the following method: calculat the distance (d),
+# to the distance. I am using the following method: calculate the distance (d),
 # miltiply each dimention (x and y) by the value ((1/d) * degree) I then assign
 # the color values from the new pixel location to the origional pixel location.
 # ------------------------------------------------------------------------------
@@ -35,47 +38,102 @@ function distance(xa,ya, xb,yb)
 end
 # ==============================================================================
 # Calculate a lens effect: x = ((oy-x)*(1/d) * degree) and y = ((oy-y)*(1/d) * degree)
+# ------------------------------------------------------------------------------
+# Let's pre-calculate the lens curvature  ...this should speed things up a bit.
+# Curve = [ (1/X) for X in 1:10000]
+# ------------------------------------------------------------------------------
+type Body
+    value::Float64            # value of curvature
+    curve::Array{Float64,1}   # lens curvature
+    voxels::Any               # Voxels image of body
+    class::String
+    x::Float64                # x origon
+    y::Float64                # y origon
+    z::Float64                # z origon
+    Body(value, x, y, z) = new(value,Float64[],0, "", x, y, z)
+end
+
+# ------------------------------------------------------------------------------
+type Space
+    bodies::Array{Body,1}     # lens curvature
+    limit::Float64            # the size of our universe
+    Space() = new([],100000)
+    Space(limit) = new([],limit)
+end
+# ------------------------------------------------------------------------------
+galaxies = ["E0.jpg", "E3.jpg", "E7.jpg", "S0.jpg", "Sa.jpg", "Sb.jpg", "SBa.jpg", "SBb.jpg", "SBc.jpg", "Sc.jpg"]
+# ------------------------------------------------------------------------------
+function createRandomBodies(space, number, bounds) # 1575
+    Z = rand( 1:bounds, number) # Create Z coords before anything else
+    sort!(Z) # Now sort them for depth
+
+    for i in 1:number
+        # Eventually the weight "10" will have to be correctly calculated!
+        createBody(space, 50, rand(galaxies), rand(1:bounds), rand(1:bounds), Z[i])
+    end
+
+end
+# ------------------------------------------------------------------------------
+function createBody(body, weight, class, x, y, z)
+    blackHole = Body(convert(Float64, weight), convert(Float64, x), convert(Float64, y), convert(Float64, z))
+    blackHole.curve = [ (1/X) for X in 1:10000] * weight
+    push!(body.bodies, blackHole )
+end
 # ==============================================================================
-function warp(x,y, ox,oy, degree)
-    dx, dy = round(Int64, x), round(Int64, y)
-        D = distance(ox,oy, x,y)
+# function warp(x,y, ox,oy, degree)
+function warp(x,y, body) # TODO: later make this work with slices (that is to say vector fields)
+    ox, oy = body.x, body.y
+        dx, dy = (ox - x), (oy - y) # Get 1D, 1D distance
+        D = sqrt( (dx * dx) + (dy * dy)  ) # Get 2D distance
+        (D==0) && return (0,0) # No point in doing anything if it's zero
 
-        D==Inf ? d=0 : d = D              # 1/0 = Inf (infinit) let's make that 0 instead
-        if D!=0                           # No point in doing anything if it's zero
-            d = (1/d) * degree;           #
-        end
-
-# cx,cy is the origion so (cx-x), for example, gets the +/- x distance from origion.
-        dx = (ox-x)*d
-        dy = (oy-y)*d
-
-    # round these to Ints because they'll have to translate to pixel coords.
-    return (round(Int64,x+dx),round(Int64,y+dy)) # hmmm.. I don't remember why: x+dx, y+dy
+        d = body.curve[round(Int64,D)];
+        X,Y   =  round(Int64,x+(dx*d)), round(Int64,y+(dy*d))
+        #Xr,Yr =  reinterpret(N0f8, round(UInt8, (X%1)*255)) , reinterpret(N0f8, round(UInt8, (Y%1)*255)) # get digits after point. This is for getting subpixel quality.
+    return (X,Y)
 end
 # ==============================================================================
 # Apply function warp() to all pixels:
 # ==============================================================================
-function paintThis()
-    for x in 1:width
-        for y in 1:height
+function paintThis(space)
 
-            # The last value determins the degree of lensing!
-            X, Y = warp(x,y, halfWidth, halfHeight-8, 200)
+    for x in 1:width, y in 1:height
 
-                if Y < height && Y > 0 && X < width && X > 0
-                    imgc[x,y] =  buffer[X,Y]
-                else
-                    # plot black pixel because the value falls outside the image's bounds
-                    imgc[x,y] = RGB(0,0,0)
-                end
+        X, Y = x,y
+        for plane in 1:length(space.bodies)
+            X, Y = warp(X, Y, space.bodies[plane])
+
+            if Y > height || Y < 1 || X > width || X < 1 #     || Z > space.limit || Z < 1
+                imgc[x,y] = RGB(.5,0,.5) # plot black pixel because the value falls outside the bounds of our universe
+                break
+            end
+
+            # TODO: test for intersection of ray with body, paint pixel and break.
+            if  plane == length(space.bodies)
+                 imgc[x,y] =  buffer[X,Y] # Simple way
+            end
         end
+
+
     end
+    imshow(imgc, aspect=:auto) # show the resulting image
+    println("W: $(width),  H:$(height) ")
+    # println(imgc)
 end
 # ==============================================================================
 
 
-paintThis()
-imshow(imgc, aspect=:auto)
+
+
+
+
+
+space = Space()
+createRandomBodies(space, 25, 1575)
+# createBody(space, 100, 's0', halfWidth, halfHeight*.43, 250)
+# createBody(space, 200, 's0', halfWidth, halfHeight-8, 10)
+paintThis(space)
+
 
 end # module
 
@@ -135,3 +193,4 @@ end # module
 #
 # I see no reason that I cannot accomplish this!
 # ------------------------------------------------------------------------------
+
